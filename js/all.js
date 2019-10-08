c240375671cf218aefeadc6e5fca75159092b8c6
Sealman <castle2668@gmail.com>
2019-10-08 16:52:14 +0800

    edit all.js

--- js/all.js
+++ js/all.js
@@ -14,7 +14,7 @@ for (let i = 2; i < 10; i++) {
     leftBox[i-2].innerHTML = '<h3 class="sub-title">' + i + '</h3>' + leftStr;
     leftStr = ' ';
     
-    // 印出右側內容
+    // 
     for (let j = 4; j < 10; j++) {
         let content = '<span class="sub-item">' + i + ' x ' + j + ' = ' + i * j + '</span></div>';
         rightStr += content;
