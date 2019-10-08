86733c70998df60396349d3ab345736677bba624
Sealman <castle2668@gmail.com>
2019-10-08 16:13:07 +0800

    update index

---
+++ js/all.js
@@ -0,0 +1,24 @@
+// let item = document.querySelectorAll('.item');
+// 宣告左側 Box
+let leftBox = document.querySelectorAll('.item .leftBox');
+let leftStr = ' ';
+// 宣告右側 Box
+let rightStr = ' ';
+let rightBox = document.querySelectorAll('.item .rightBox');
+
+for (let i = 2; i < 10; i++) {
+    // 印出左側內容
+    for (let j = 1; j < 4; j++) {
+        let content = '<span class="sub-item">' + i + ' x ' + j + ' = ' + i * j + '</span>';
+        leftStr += content;
+    }
+    leftBox[i-2].innerHTML = '<h3 class="sub-title">' + i + '</h3>' + leftStr;
+    leftStr = ' ';
+    // 印出右側內容
+    for (let j = 4; j < 10; j++) {
+        let content = '<span class="sub-item">' + i + ' x ' + j + ' = ' + i * j + '</span></div>';
+        rightStr += content;
+    }
+    rightBox[i-2].innerHTML = rightStr;
+    rightStr = '';
+}
